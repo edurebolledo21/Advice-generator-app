ca1b2964bc07114ed2bf37dcaae3b4ed470fc9a9
edurebolledo21 <edurebolledo68@gmail.com>
2023-02-01 09:06:29 -0400

    fix-js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const btn = document.querySelector("#btn-pass");
-const span = document.querySelector(".number");
+const span = document.querySelector("p");
 const parrafo = document.querySelector("h3");
 
 const getAdvice = async () => {
   const { slip } = await (await fetch("https://api.adviceslip.com/advice", { method: "GET" })).json();
   const { id, advice } = slip;
-  span.innerHTML = `${id}`;
-  parrafo.innerHTML = `${advice}`;
+  span.innerHTML = `ADVICE #${id}`;
+  parrafo.innerHTML = advice;
 };
 getAdvice();
 
-btn.addEventListener("click", async (e) => {
-  const { slip } = await (await fetch("https://api.adviceslip.com/advice", { method: "GET" })).json();
-  const { id, advice } = slip;
-  span.innerHTML = `${id}`;
-  parrafo.innerHTML = `${advice}`;
-});
+btn.addEventListener("click", getAdvice);
